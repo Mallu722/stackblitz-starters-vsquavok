fd3d757d29fdc7b20fff8995d439363b2d70a3d2
Mallikarjun722 <anandhiremath979@gmail.com>
2025-01-15 14:39:26 +0530

    change

---
+++ config.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();  
+
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
